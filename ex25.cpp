b6df2e374ca8a0d6f9f056e6c1e09bef3e0a5aac
veda samhitha <11249a303@kanchiuniv.ac.in>
2025-11-09 23:38:35 -0800

    Add files via upload

---
+++ ex25.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+cout<<"Content-type:text/html\n\n";
+cout<<"<htm><body>";
+cout<<"<h2>Hello from C++ Web CGI Program!</h2>";
+cout<<"</body></html>";
+return 0;
+}
